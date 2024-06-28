ed46a5d3ab2f688fd9ddd000116dbe053c1b64d6
JoakimSP <joakim@spprod.se>
2024-06-28 21:51:08 +0200

    remove query

--- src/app/lists/page.tsx
+++ src/app/lists/page.tsx
@@ -1,17 +1,9 @@
 import ListUI from "../_components/lists/emailListUI"
 import CreateList from "../_components/lists/createList"
-import { api } from "~/trpc/server"
 
 type Props = {}
 
 export default async function page({}: Props) {
-    const listData = await api.lists.read()
-
-    const data = listData.map((key, index) => {
-        return (
-            console.log(key)
-        )
-    })
 
   return (
     <div>
