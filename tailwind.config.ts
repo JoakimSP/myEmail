964da0dd51cb9bc7bafb833686b5e08c40af580e
JoakimSP <joakim@spprod.se>
2024-06-28 18:13:46 +0200

    Add daysyUI theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,7 @@
 import { type Config } from "tailwindcss";
-import { fontFamily } from "tailwindcss/defaultTheme";
+import theme, { fontFamily } from "tailwindcss/defaultTheme";
+import daisyui from "daisyui"
+
 
 export default {
   content: ["./src/**/*.tsx"],
@@ -10,5 +12,10 @@ export default {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    daisyui,
+  ],
+  daisyui: {
+    themes: ["light", "dark", "nord"],
+  },
 } satisfies Config;
