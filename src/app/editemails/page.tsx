16c02aa9edd9dd27c7100e63cf186b466df9203e
JoakimSP <joakim@spprod.se>
2024-06-30 21:39:03 +0200

    Get list data and use as prop

--- src/app/editemails/page.tsx
+++ src/app/editemails/page.tsx
@@ -1,12 +1,13 @@
 import CreateEmail from "../_components/email/createEmail"
+import { api } from "~/trpc/server"
 
 type Props = {}
 
 export default async function Page({ }: Props) {
-
+    const lists = await api.lists.read()
     return (
         <div className="m-4">
-            <CreateEmail/>
+            <CreateEmail lists={lists}/>
         </div>
     )
 }
