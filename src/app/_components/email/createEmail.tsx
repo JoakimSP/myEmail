e6d4916531ffb79759fe231a2411ea33d84ba6e0
JoakimSP <joakim@spprod.se>
2024-07-01 15:35:10 +0200

    Update success-notification to toast

--- src/app/_components/email/createEmail.tsx
+++ src/app/_components/email/createEmail.tsx
@@ -1,11 +1,8 @@
 'use client'
 
-import React, { useState } from 'react'
 import { api } from "~/trpc/react";
-
-
-
-type statusType = "successfully added new email" | "could not add new email" | "pending";
+import { toast } from 'react-toastify';
+import { useRouter } from "next/navigation";
 
 interface Ilist {
     id: number;
@@ -20,20 +17,15 @@ type createEmailProps = {
 
 
 const CreateEmail: React.FC<createEmailProps> = ({ lists }) => {
-    const [status, setStatus] = useState<statusType | null>(null)
+    const router = useRouter()
 
     const createEmail = api.contacts.create.useMutation({
         onSuccess: () => {
-            setStatus('successfully added new email')
-            setTimeout(() => {
-                setStatus(null);
-            }, 3000);
+            toast.success("successfully added new email")
+            router.refresh()
         },
         onError: () => {
-            setStatus('could not add new email');
-            setTimeout(() => {
-                setStatus(null);
-            }, 3000);
+            toast.success("Could not add new email")
         }
     })
 
@@ -68,22 +60,8 @@ const CreateEmail: React.FC<createEmailProps> = ({ lists }) => {
                         )
                     })}
                 </select>
-                <button type='submit' className="btn btn-active btn-primary">Primary</button>
+                <button type='submit' className="btn btn-active btn-primary">Add +</button>
             </form>
-
-            <div>
-                {status && (
-                    <div className="flex items-center p-4 mb-4 text-sm text-green-800 border border-green-300 rounded-lg bg-green-50 dark:bg-gray-800 dark:text-green-400 dark:border-green-800" role="alert">
-                        <svg className="flex-shrink-0 inline w-4 h-4 me-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                            <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z" />
-                        </svg>
-                        <span className="sr-only">Info</span>
-                        <div>
-                            <span className="font-medium">{status}</span>
-                        </div>
-                    </div>
-                )}
-            </div>
         </>
     )
 }
