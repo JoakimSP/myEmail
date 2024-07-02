f62468bdace392ccfabe1555813db9d9441fd06c
JoakimSP <joakim@spprod.se>
2024-07-02 20:08:07 +0200

    Add function to add emails from excel file

---
+++ src/app/_components/email/addEmailsFromList.tsx
@@ -0,0 +1,86 @@
+'use client'
+
+import { toast } from "react-toastify"
+import { api } from "~/trpc/react"
+import readXlsxFile from 'read-excel-file'
+import { useState } from "react"
+
+interface Ilist {
+  id: number;
+  name: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+type createEmailProps = {
+  lists: Ilist[] | undefined
+}
+
+const AddEmailsFromList: React.FC<createEmailProps> = ({ lists }) => {
+  const [emails, setEmails] = useState<string[]>([]);
+
+  const createEmail = api.contacts.create.useMutation({
+    onSuccess: () => {
+      console.log("successfully added new email")
+    },
+    onError: () => {
+      toast.error("Could not add new email")
+    }
+  })
+
+  const handleAddEmails = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const form = e.currentTarget;
+    const input = form.elements.namedItem('fileInput') as HTMLInputElement;
+    const select = form.elements.namedItem('listOption') as HTMLSelectElement;
+    const file = input?.files?.[0]; 
+    const selectedList = Number(select.value); 
+
+    if (file) {
+      console.log(file);
+      const rows = await readXlsxFile(file);
+      const emailArray: string[] = [];
+      rows.forEach(row => {
+        row.forEach(cell => {
+          if (typeof cell === 'string' && cell.includes('@')) {
+            emailArray.push(cell); // Add cell to email array if it contains '@'
+          }
+        });
+      });
+      setEmails(emailArray);
+
+      let loopCounter = 0
+      emailArray.forEach((email, index, array) => {
+        const data = {
+          name: "Unknown", 
+          email: email,
+          address: "", 
+          phoneNumber: 0, 
+          emailList: selectedList 
+        };
+        createEmail.mutate(data)
+        loopCounter++
+        if (loopCounter === array.length){
+          toast.success("successfully added new email")
+        }
+      })
+    }
+  }
+
+  return (
+    <>
+      <h1 className='text-5xl font-extrabold dark:text-white text-center'>Add multiple Emails from Excel</h1>
+      <form onSubmit={handleAddEmails} className="flex flex-col items-center justify-center gap-2">
+        <input type="file" name="fileInput" className="file-input file-input-bordered w-full max-w-xs" accept=".xlsx, .xls" />
+        <select name="listOption" className="select select-primary w-full max-w-xs">
+          {lists?.map((list, index) => (
+            <option key={index} value={list.id}>{list.name}</option>
+          ))}
+        </select>
+        <button type="submit" className="btn btn-primary">Submit</button>
+      </form>
+    </>
+  )
+}
+
+export default AddEmailsFromList
--- src/app/editemails/page.tsx
+++ src/app/editemails/page.tsx
@@ -1,4 +1,5 @@
 import CreateEmail from "../_components/email/createEmail"
+import AddEmailsFromList from "../_components/email/addEmailsFromList"
 import { api } from "~/trpc/server"
 
 type Props = {}
@@ -6,6 +7,9 @@ type Props = {}
 export default async function Page({ }: Props) {
     const lists = await api.lists.readAll()
     return (
+        <>
         <CreateEmail lists={lists} />
+        <AddEmailsFromList lists={lists}/>
+        </>
     )
 }
