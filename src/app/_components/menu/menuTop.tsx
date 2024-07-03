0a7870673c3801e1bdf7406aad3b5c1400c77160
JoakimSP <joakim@spprod.se>
2024-07-03 14:21:52 +0200

    Add user information menu

---
+++ src/app/_components/menu/menuTop.tsx
@@ -0,0 +1,43 @@
+import { SignInButton, SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
+
+type Props = {}
+
+export default function MenuTop({ }: Props) {
+    return (
+        <div className="navbar bg-base-100">
+            <div className="flex-1">
+                <a className="btn btn-ghost text-xl">My Email</a>
+            </div>
+            <div className="flex-none gap-2">
+                
+                <div className="dropdown dropdown-end">
+                   {/*  <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+                        <div className="w-10 rounded-full">
+                            <img
+                                alt="Tailwind CSS Navbar component"
+                                src={user?.imageUrl} />
+                        </div>
+                    </div> */}
+                    <SignedOut>
+                    <SignInButton />
+                </SignedOut>
+                <SignedIn>
+                    <UserButton />
+                </SignedIn>
+                    <ul
+                        tabIndex={0}
+                        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+                        <li>
+                            <a className="justify-between">
+                                Profile
+                                <span className="badge">New</span>
+                            </a>
+                        </li>
+                        <li><a>Settings</a></li>
+                        <li><a>Logout</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    )
+}
