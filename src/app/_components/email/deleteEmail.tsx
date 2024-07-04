72ac680e72f7adab236e322c65d9790c72717eda
JoakimSP <joakim@spprod.se>
2024-07-04 22:03:58 +0200

    Update UI on deleting email

--- src/app/_components/email/deleteEmail.tsx
+++ src/app/_components/email/deleteEmail.tsx
@@ -3,26 +3,39 @@
 import { api } from "~/trpc/react"
 import { toast } from "react-toastify"
 import { useRouter } from "next/navigation"
+import { useContext } from "react"
+import { SetEmailsContext } from "../lists/listDetailUI"
 
-type Props = { id: string }
+interface Props {
+    id: string,
+}
 
 export default function DeleteEmail({ id }: Props) {
+    const setEmail = useContext(SetEmailsContext)
     const router = useRouter()
     const deleteEmail = api.contacts.delete.useMutation({
-        onSuccess: () => {
+        onSuccess: (data) => {
             toast.success("Email successfully deleted")
+            if (setEmail) {
+                setEmail((prev) => {
+                    if (prev) {
+                        return prev.filter((email) => email.id !== data.id);
+                    }
+                    return prev;
+                });
+            }
             router.refresh()
         },
-        onError : () => {
+        onError: () => {
             toast.error("Not able to delete email")
         }
     })
 
-    const HandleDeleteEmail = (id: string) : void => {
-       const userConfirmed = window.confirm("Are you sure you want to delete this email? This cannot be reversed")
-       if(userConfirmed){
-        deleteEmail.mutate(id)
-       }
+    const HandleDeleteEmail = (id: string): void => {
+        const userConfirmed = window.confirm("Are you sure you want to delete this email? This cannot be reversed")
+        if (userConfirmed) {
+            deleteEmail.mutate(id)
+        }
         else {
             return
         }
--- src/app/_components/lists/listDetailUI.tsx
+++ src/app/_components/lists/listDetailUI.tsx
@@ -1,7 +1,6 @@
 'use client'
-
 import DeleteEmail from "../email/deleteEmail";
-import { useState } from "react";
+import { useState, createContext, SetStateAction } from "react";
 
 interface Icontacts {
     id: string;
@@ -22,8 +21,10 @@ type props = {
     listCount: number | undefined
 }
 
+export const SetEmailsContext = createContext<React.Dispatch<SetStateAction<contactsType>> | null>(null)
 export default function ListDetailUI({ contacts, listName, listCount }: props) {
     const [emails, setEmails] = useState<contactsType>(contacts)
+
 
     const handleSearchEmails = (e: React.FormEvent<HTMLFormElement>): void => {
         e.preventDefault();
@@ -40,59 +41,61 @@ export default function ListDetailUI({ contacts, listName, listCount }: props) {
 
     return (
         <>
-            <h1 className='text-5xl font-extrabold dark:text-white'>{listName}</h1>
-            <h4>Total email count: {listCount}</h4>
-            <div>
-                <h4> Search Email</h4>
-                <form className="flex flex-col items-center justify-center gap-2" onSubmit={handleSearchEmails}>
-                    <label className="input input-bordered flex items-center gap-2">
-                        <input name="query" type="text" className="grow" placeholder="Search" />
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 16 16"
-                            fill="currentColor"
-                            className="h-4 w-4 opacity-70">
-                            <path
-                                fillRule="evenodd"
-                                d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
-                                clipRule="evenodd" />
-                        </svg>
-                    </label>
-                    <div>
-                        <button typeof="submit" className="btn btn-active btn-primary">Search</button>
-                    </div>
-                </form>
-            </div>
-            <div className="overflow-x-auto">
-                <table className="table">
-                    {/* head */}
-                    <thead>
-                        <tr>
-                            <th>Id</th>
-                            <th>Name</th>
-                            <th>Email</th>
-                            <th>Delete</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {/* row 1 */}
+            <SetEmailsContext.Provider value={setEmails}>
+                <h1 className='text-5xl font-extrabold dark:text-white'>{listName}</h1>
+                <h4>Total email count: {listCount}</h4>
+                <div>
+                    <h4> Search Email</h4>
+                    <form className="flex flex-col items-center justify-center gap-2" onSubmit={handleSearchEmails}>
+                        <label className="input input-bordered flex items-center gap-2">
+                            <input name="query" type="text" className="grow" placeholder="Search" />
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                viewBox="0 0 16 16"
+                                fill="currentColor"
+                                className="h-4 w-4 opacity-70">
+                                <path
+                                    fillRule="evenodd"
+                                    d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
+                                    clipRule="evenodd" />
+                            </svg>
+                        </label>
+                        <div>
+                            <button typeof="submit" className="btn btn-active btn-primary">Search</button>
+                        </div>
+                    </form>
+                </div>
+                <div className="overflow-x-auto">
+                    <table className="table">
+                        {/* head */}
+                        <thead>
+                            <tr>
+                                <th>Id</th>
+                                <th>Name</th>
+                                <th>Email</th>
+                                <th>Delete</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {/* row 1 */}
 
-                        {emails?.map((contact, index) => {
-                            return (
-                                <tr key={contact.id}>
-                                    <th>{index}</th>
-                                    <td>{contact.name}</td>
-                                    <td>{contact.email}</td>
-                                    <td>
-                                        <DeleteEmail id={contact.id} />
-                                    </td>
-                                </tr>
-                            )
-                        })
-                        }
-                    </tbody>
-                </table>
-            </div>
+                            {emails?.map((contact, index) => {
+                                return (
+                                    <tr key={contact.id}>
+                                        <th>{index}</th>
+                                        <td>{contact.name}</td>
+                                        <td>{contact.email}</td>
+                                        <td>
+                                            <DeleteEmail id={contact.id} />
+                                        </td>
+                                    </tr>
+                                )
+                            })
+                            }
+                        </tbody>
+                    </table>
+                </div>
+            </SetEmailsContext.Provider>
         </>
     )
 }
